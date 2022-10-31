f902cce6f89f9d8d9047b9dbbb3bd7be35f6f032
Jubair Ahmed Nabin <ahmednabin.csecuet@gmail.com>
2022-10-31 18:36:45 +0600

    Remove unnecessary import in leftMenu.js

--- client/src/components/Navbar/LeftMenu.js
+++ client/src/components/Navbar/LeftMenu.js
@@ -1,14 +1,11 @@
 import React, { Component } from 'react';
 import { Menu } from 'antd';
-const SubMenu = Menu.SubMenu;
-const MenuItemGroup = Menu.ItemGroup;
+// const SubMenu = Menu.SubMenu;
+// const MenuItemGroup = Menu.ItemGroup;
 class LeftMenu extends Component {
   render() {
     return (
    <Menu mode="horizontal" className='nav-item'>
-       <Menu.Item key="mail">
-          <a href="">Home</a>
-        </Menu.Item>
         <Menu.Item key="alipay">
           <a href="">Contact Us</a>
         </Menu.Item>
