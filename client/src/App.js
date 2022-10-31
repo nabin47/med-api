dec45410199f15985e6e012dc6d34512f8e2952c
Jubair Ahmed Nabin <ahmednabin.csecuet@gmail.com>
2022-10-31 14:59:40 +0600

    Add router in App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,31 +1,24 @@
-import React, {useState} from 'react';
-import Navbar from './components/Navbar/Navbar'
-import SearchbarDropdown from './components/SearchbarDropdown';
-import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.css';
-import DataTable from './DataTable';
-
-const defaultOptions = []
-for ( let i = 0; i < 10; i++) {
-    defaultOptions.push(`Id ${i}`);
-    defaultOptions.push(`Medicine ${i}`);
-    defaultOptions.push(`Ingredients ${i}`);
-}
-
+import Login from './Login'
+import Homepage from './Homepage';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import DataTable from './DataTable/DataTable';
 function App() {
-  const [options, setOptions] = useState([]);
-
-  const onInputChange = (event) => {
-    setOptions(defaultOptions.filter(option => option.includes(event.target.value)));
-  } 
-  
   return (
+    <Router>
     <div className="App">
-      <DataTable />
-      <Navbar />
-      <SearchbarDropdown options={options} onInputChange={onInputChange}/>
-      <button className='btn btn-primary'>Search</button>
+      {/*<Homepage/>*/}
+      <div>
+      <Routes>
+              <Route exact path="/" element={<Homepage />} />
+              <Route path="/Login" element={<Login />} />
+              <Route path="/DataTable/DataTable" element={<DataTable />} />
+       </Routes>
+  </div>
+      
     </div>
+   
+    </Router>
   );
 }
 
