516c898db45fd9f0e92fc29f1df8093ed766d7d4
Jubair Ahmed Nabin <ahmednabin.csecuet@gmail.com>
2022-10-31 14:58:44 +0600

    Add page routing

---
+++ client/src/Login.js
@@ -0,0 +1,85 @@
+import React from 'react'
+import 'antd/dist/antd.css';
+import './Login.css';
+import { Form, Input, Button, Checkbox } from 'antd';
+import { Link, useNavigate } from 'react-router-dom';
+
+import Navbar from './components/Navbar/Navbar';
+
+function Login() {
+  
+  const onFinish = (values) => {
+    console.log('Success:', values);
+    alert('Login Successfully')
+  };
+
+  const navigate = useNavigate();
+
+  const navigateToDataTable = () => {
+    navigate('/DataTable/DataTable')
+  }
+
+  return (
+    <div>
+        <Navbar />
+        
+        <Form
+        name="normal_login"
+        onFinish={onFinish}
+        className="login-form"
+        initialValues={{
+            remember: true,
+        }}
+        >
+        <Form.Item
+            name="email"
+            rules={[
+            {
+                required: true,
+                message: 'Please input your Email!'
+            },
+            {
+                pattern:  /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,4})+$/,
+                message: 'Your Email is invalid!'
+            },
+            ]}
+            hasFeedback
+        >
+            <Input placeholder="Email" />
+        </Form.Item>
+        <Form.Item
+            name="password"
+            rules={[
+            {
+                required: true,
+                message: 'Please input your Password!',
+            },
+            { min: 8 },
+            ]}>
+            <Input
+            type="password"
+            placeholder="Password"
+            />
+        </Form.Item>
+
+        <Form.Item>
+            <Form.Item name="remember" valuePropName="checked" noStyle>
+            <Checkbox>Remember me</Checkbox>
+            </Form.Item>
+
+            <a className="login-form-forgot" href="forgot.html">
+            Forgot password
+            </a>
+        </Form.Item>
+
+        <Form.Item>
+            <Button type="primary" htmlType="submit" className="login-form-button" onClick={navigateToDataTable}>
+            Log in
+            </Button>
+        </Form.Item>
+        </Form>
+    </div>
+
+  );
+};
+export default Login;
