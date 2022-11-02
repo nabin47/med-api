7763aa55a8a82ac2bfe774104b64beb19e2993a3
Jubair Ahmed Nabin <ahmednabin.csecuet@gmail.com>
2022-11-02 15:25:33 +0600

    Add Details page

--- client/src/Details.js
+++ client/src/Details.js
@@ -3,6 +3,7 @@ import { Link, useParams, useLocation } from 'react-router-dom';
 import Navbar from './components/Navbar/Navbar';
 import './App.css';
 import Footer from './components/Footer/Footer';
+import './Details.css'
 
 const Details = ()=>{
     const [item,setItem]= React.useState({
@@ -16,30 +17,38 @@ const Details = ()=>{
         SideEffects:'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.', 
         Description:'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
     })
-    const {type} = useParams();
+    {/*const {type} = useParams();
     const stateParamVal = useLocation.state.stateParam;
-    console.log(type);
+console.log(type);*/}
 
     return(
        <div>
         <Navbar/>
-        <h1 style={{fontFamily:"Arial, Helvetica, sans-serif", marginLeft:"20px", marginRight:"20px"}}>{item.GenericName}</h1>
-        <h2 className='heading'>Strength</h2>
-        <p className='prod-description'>{item.Strength}</p>
-        <h2 className='heading'> Manufacturer</h2>
-        <p className='prod-description' >{item.Manufacturer}</p>
-        <h2 className='heading' >UnitPrice</h2>
-        <p className='prod-description' >{item.UnitPrice}</p>
-        <h2 className='heading' >Indication</h2>
-        <p className='prod-description' >{item.Indication}</p>
-        <h2 className='heading' >Description</h2>
-        <p className='prod-description' >{item.Description}</p>
-        <h2 className='heading' >Doses</h2>
-        <p className='prod-description' >{item.Doses}</p>
-        <h2 className='heading' >SideEffects</h2>
-        <p className='prod-description' >{item.SideEffects}</p>
-        <h2 className='heading' >Precautions</h2>
-        <p className='prod-description' >{item.precautions}</p>
+        <div class="medicine">
+            <div class="medicine-description">
+              <p class="generic-name">{item.GenericName}</p>
+              <p class="medicine-attr">{item.Strength}</p>
+              <p class="medicine-attr">{item.Manufacturer}</p>
+              <p class="medicine-attr">{item.UnitPrice}</p>
+            </div>
+            <div>
+              <h4 class="med-heading">Indication</h4>
+              <p class="description">{item.Indication}</p>
+              <h4 class="med-heading">SideEffects</h4>
+              <p class="description">{item.SideEffects}</p>
+              <h4 class="med-heading">Description</h4>
+              <p class="description">{item.Description}</p>
+              <h4 class="med-heading">Precautions</h4>
+              <div class="description">
+              <ul class>
+                <li>{item.precautions}</li>
+                <li>{item.precautions}</li>
+                <li>{item.precautions}</li>
+                <li>{item.precautions}</li>
+              </ul>
+              </div>
+            </div>
+           </div>
 
         <Footer />
 
